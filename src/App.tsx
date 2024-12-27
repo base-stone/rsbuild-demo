5abeed1e64376f5fa6877450c10e376c5914d972
yeliang <yeliang2426@shengtian.com>
2024-12-27 17:25:03 +0800

    update button

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
-      <Button>按钮</Button>
+      <Button>submit</Button>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
